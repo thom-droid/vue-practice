370bc059c932bcc935d89202cc36455064079d21
thom-window <hsbang.thom@gmail.com>
2023-08-21 00:10:19 +0900

    watch

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -3,18 +3,45 @@ const app = Vue.createApp({
     return {
       counter: 10,
       name: "",
-      lastName: "Schwarz",
+      lastName: "",
       confirmedName: "",
+      // fullName: "",
     };
+  },
+  watch: {
+    counter(value) {
+      if (value > 50) {
+        const vm = this;
+        setTimeout(function () {
+          vm.counter = 0;
+        }, 2000);
+      }
+    },
+    // name(value) {
+    //   console.log("name property is being wathced");
+    //   if (value === "") {
+    //     this.fullName = "";
+    //   } else {
+    //     this.fullName = value + " " + this.lastName;
+    //   }
+    // },
+    // lastName(value) {
+    //   if (value === "") {
+    //     this.fullName = "";
+    //   } else {
+    //     this.fullName = this.name + " " + value;
+    //   }
+    // },
   },
   computed: {
     outputFullName() {
       // this is going to run every time the page is rendered
       console.log("this is running every time v-dom is updated");
-      if (this.name) {
-        return this.name + " Schwarz";
+      if (!this.name || !this.lastName) {
+        return "";
       }
-      return "";
+
+      return this.name + " " + this.lastName;
     },
   },
   methods: {
