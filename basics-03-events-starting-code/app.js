e08d708a54dfcf4d10eff97a588901c005aff1be
thom-window <hsbang.thom@gmail.com>
2023-08-20 19:38:16 +0900

    v-on modifier

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -4,9 +4,16 @@ const app = Vue.createApp({
       counter: 0,
       name: '',
       lastName: 'Schwarz',
+      confirmedName: '',
     };
   },
   methods: {
+    setConfirmedName(){
+      this.confirmedName = this.name;
+    },
+    submitForm(){
+      alert('submitted!');
+    },
     setName(event, lastName) {
       this.name = event.target.value + ' ' + lastName;
     },
