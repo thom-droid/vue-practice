b9fc7a0c1a84b3263d933c0bb80fac9b28911b9b
thom-window <hsbang.thom@gmail.com>
2023-08-20 23:03:43 +0900

    computed options

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -2,29 +2,49 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 10,
-      name: '',
-      lastName: 'Schwarz',
-      confirmedName: '',
+      name: "",
+      lastName: "Schwarz",
+      confirmedName: "",
     };
   },
+  computed: {
+    outputFullName() {
+      // this is going to run every time the page is rendered
+      console.log("this is running every time v-dom is updated");
+      if (this.name) {
+        return this.name + " Schwarz";
+      }
+      return "";
+    },
+  },
   methods: {
-    setConfirmedName(){
+    // outputFullName() {
+    //   // this is going to run every time the page is rendered
+    //   console.log("this is running every time v-dom is updated");
+    //   if (this.name) {
+    //     return this.name + " Schwarz";
+    //   }
+    //   return "";
+    // },
+    setConfirmedName() {
       this.confirmedName = this.name;
     },
-    submitForm(){
-      alert('submitted!');
+    submitForm() {
+      alert("submitted!");
     },
-    setName(event, lastName) {
-      this.name = event.target.value + ' ' + lastName;
+    setName(event) {
+      this.name = event.target.value;
     },
     increment(num) {
       this.counter += num;
     },
     subtract(num) {
       this.counter -= num;
-    }
-
-  }
+    },
+    resetInput() {
+      this.name = "";
+    },
+  },
 });
 
-app.mount('#events');
+app.mount("#events");
