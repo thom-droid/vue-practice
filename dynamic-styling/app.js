74ca27b34243e3983744a65d48b4fe248cddad6f
thom-window <hsbang.thom@gmail.com>
2023-08-23 00:35:21 +0900

    dynamic styling- binding class as array & computed

--- dynamic-styling/app.js
+++ dynamic-styling/app.js
@@ -6,6 +6,11 @@ const app = Vue.createApp({
       boxCSelected: false,
     };
   },
+  computed: {
+    computeBoxASelected() {
+      return { active: this.boxASelected };
+    },
+  },
   methods: {
     boxSelected(box) {
       if (box === "A") {
